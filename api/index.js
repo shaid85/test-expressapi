ecadd6ce7ccba2b00ec540cb161393b98a667c2e
Shaid Islam <shaid85@gmail.com>
2025-06-26 02:53:04 +0600

    update 5

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// api/index.js
+import app from '../src/server.js';   // path to your compiled or source Express app
+
+export default app;                   //  <-- this is the serverless handler Vercel needs
+
+
